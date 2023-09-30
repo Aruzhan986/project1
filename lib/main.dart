1e6f66b95b341949d83b8a9a7f1e794e3c515088
Baltabay Aruzhan <baltabay.aruzhan@bk.ru>
2023-09-30 15:57:42 +0600

    first

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screen/firstscreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -6,16 +7,72 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.pink,
+        title: Text('Flutter Moon'),
       ),
-      body: Container(
-          color: Colors.purple, child: Text("Baltabay Aruzhans Home Page")),
-    ));
+      body: ListView(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            child: Text(
+              'Hello flutter from metanit.com',
+              style: TextStyle(fontSize: 24.0),
+            ),
+          ),
+          SizedBox(height: 400.0),
+          Center(
+            child: Container(
+              alignment: Alignment.center,
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Hello Flutter',
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 400.0),
+          Center(
+            child: Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.all(16.0),
+              child: Text(
+                'Текст слева по центру',
+                style: TextStyle(fontSize: 24.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 400.0),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const firstcreen()),
+                      );
+                    },
+                    child: const Text('Следующая страница'))
+              ],
+            ),
+          ),
+          SizedBox(height: 400.0),
+        ],
+      ),
+    );
   }
 }
